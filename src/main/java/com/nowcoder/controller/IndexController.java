b4ad4e03edc41451e6b6b643be321811a057071f
kzsm <gqfgn@163.com>
2017-08-10 02:30:22 +0800

    2017年8月10日01:52:52

--- src/main/java/com/nowcoder/controller/IndexController.java
+++ src/main/java/com/nowcoder/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.nowcoder.controller;
 
+import com.nowcoder.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.xml.ws.RequestWrapper;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/8/10.
@@ -27,8 +33,16 @@ public class IndexController {
     }
 
     @RequestMapping(path = {"/vm"})
-    public String news(Model model){
-        model.addAttribute("value1","vv1");
+    public String news(Model model) {
+        model.addAttribute("value1", "vv1");
+        List<String> strList = Arrays.asList(new String[]{"red", "yello", "blue"});
+        Map<Integer, Integer> intMap = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            intMap.put(Integer.valueOf(i * i), Integer.valueOf(i * i * i));
+        }
+        model.addAttribute("strList",strList);
+        model.addAttribute("intMap",intMap);
+        model.addAttribute("user",new User("xiaoming",25));
         return "news";
     }
 }

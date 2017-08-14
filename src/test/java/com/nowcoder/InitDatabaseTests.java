c1520d52836560c17e440463aa4092c23767164b
kzsm <gqfgn@163.com>
2017-08-15 02:00:32 +0800

    2017年8月15日01:01:57

--- src/test/java/com/nowcoder/InitDatabaseTests.java
+++ src/test/java/com/nowcoder/InitDatabaseTests.java
@@ -1,6 +1,8 @@
 package com.nowcoder;
 
+import com.nowcoder.dao.NewsDAO;
 import com.nowcoder.dao.UserDAO;
+import com.nowcoder.model.News;
 import com.nowcoder.model.User;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
 import java.util.Random;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,8 +22,11 @@ public class InitDatabaseTests {
     @Autowired
     UserDAO userDAO;
 
+    @Autowired
+    NewsDAO newsDAO;
+
     @Test
-    public void contextLoads() {
+    public void inDate() {
         Random random = new Random();
         for (int i = 0; i < 11; ++i) {
             User user = new User();
@@ -32,11 +38,27 @@ public class InitDatabaseTests {
 
             userDAO.addUser(user);
 
+            //
+            News news = new News();
+            news.setCommentCount(i);
+            //
+            Date date=new Date();
+            date.setTime(date.getTime()+1000*3600*5*i);
+            news.setCreatedDate(date);
+            news.setImage(String.format("http://image.nowcoder.com/head/%dm.png", random.nextInt(1000)));
+            news.setLikeCount(i+1);
+            news.setUserId(i+1);
+            news.setTitle(String.format("TITLE{%d}",i));
+            news.setLink(String.format("http://www.nowcoder.com/%d.html",i));
+            newsDAO.addNews(news);
+
             user.setPassword("newpassword");
             userDAO.updatePassword(user);
         }
 
-        Assert.assertEquals("newpassword",userDAO.selectById(2).getPassword());
+        Assert.assertEquals("newpassword", userDAO.selectById(2).getPassword());
+        userDAO.deleteById(2);
+        Assert.assertNull(userDAO.selectById(2));
     }
 
 }
